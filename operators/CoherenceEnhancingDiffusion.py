b3aacbd46c66bb836e4e77fdeadb01e2270a68c4
Filip Ilic <filip.ilic@tugraz.at>
2021-07-09 14:41:20 +0200

    Kx, Ky, and compare with convolution

--- operators/CoherenceEnhancingDiffusion.py
+++ operators/CoherenceEnhancingDiffusion.py
@@ -2,56 +2,105 @@ import torch
 import matplotlib.pyplot as plt
 from skimage import data
 from skimage.transform import resize
-
-def get_Kx_shitty(width, height):
-    mn = width*height
-    Kx = torch.zeros((mn + 2, mn + 2))
-    for col in range(mn):
-        for row in range(mn):
-            if col == row:
-                Kx[row, col - 1] = -1 / 2
-                Kx[row, col + 1] = 1 / 2
-    Kx = Kx[1:-1, 1:-1]
-    return Kx
-
-def get_Ky_shitty(width, height):
-    mn = width*height
-    Ky = torch.zeros((mn,mn))
-    for row in range(height, mn - height):
-        Ky[row, row - height] = -1 / 2
-        Ky[row, row + height] = 1 / 2
-    return Ky
+import numpy as np
+import torch.nn.functional as F
 
 def get_Kx(width, height):
     # build gradient x operator, sparse matrix. use Kx.matmul(Image), or Kx.to_dense() to view
     # all values above the main diagonal for dx
-    cidx = torch.Tensor(range(width-1)).int() + 1
-    ridx = torch.Tensor(range(height-1)).int()
+    MN = width*height
+
+    cidx = torch.Tensor(range(MN-1)).int() + 1
+    ridx = torch.Tensor(range(MN-1)).int()
     i1 = torch.stack((ridx, cidx))
-    v1 = torch.Tensor([1/2]).tile(width-1)
+    v1 = torch.Tensor([1/2]).tile(MN-1)
 
     # all values below the main diagonal for dx
-    cidx = torch.Tensor(range(width - 1)).int()
-    ridx = torch.Tensor(range(height - 1)).int() + 1
+    cidx = torch.Tensor(range(MN-1)).int()
+    ridx = torch.Tensor(range(MN-1)).int() + 1
     i2 = torch.stack((ridx, cidx))
-    v2 = torch.Tensor([-1/2]).tile(width - 1)
+    v2 = torch.Tensor([-1/2]).tile(MN-1)
 
     indices = torch.cat((i1, i2), dim=1)
     values = torch.cat((v1, v2))
 
-    Kx = torch.sparse_coo_tensor(indices, values, size=(height, width))
+    Kx = torch.sparse_coo_tensor(indices, values, size=(MN,MN))
     return Kx
 
 def get_Ky(width, height):
-    pass
+    # build gradient y operator, sparse matrix. use Ky.matmul(Image), or Ky.to_dense() to view
+    # all values above the main diagonal for dy
+    MN = width*height
 
-if __name__ == "__main__":
-    print("Coherence Enhancing Diffusion with parameters ...")
+    cidx = torch.Tensor(range(MN-1)).int() + width
+    ridx = torch.Tensor(range(MN-1)).int()
+
+    cidx = cidx[:-width]
+    ridx = ridx[:-width]
+
+    i1 = torch.stack((ridx, cidx))
+    v1 = torch.Tensor([1/2]).tile(MN-width-1)
+
+    # all values below the main diagonal for y
+    cidx = torch.Tensor(range(MN-1)).int()
+    ridx = torch.Tensor(range(MN-1)).int() + width
+
+    cidx = cidx[:-width]
+    ridx = ridx[:-width]
+
+    i2 = torch.stack((ridx, cidx))
+    v2 = torch.Tensor([-1/2]).tile(MN-width-1)
+
+    indices = torch.cat((i1, i2), dim=1)
+    values = torch.cat((v1, v2))
+
+    Ky = torch.sparse_coo_tensor(indices, values, size=(MN,MN))
+    return Ky
+
+def test_grad_operator():
     u = data.camera()
     width, height = (300, 300)
     u = resize(u, (width, height))
     u = torch.from_numpy(u).float()
+    u = torch.reshape(u, (width * height, 1))
 
+    # ----------- Gradient X direction -----------
+    # once with convolution, once with bigass op matrix
     Kx = get_Kx(width, height)
-    plt.imshow(Kx.matmul(u), cmap='gray')
+
+    cx = torch.Tensor([[ 0, 0, 0],
+                       [-1/2, 0, 1/2],
+                       [ 0, 0, 0]]).unsqueeze(0).unsqueeze(0)
+
+    conv_x_u = F.conv2d(u.reshape(1,1,width, height), cx, bias=None)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(conv_x_u.squeeze(), cmap='gray')
+    ax[0].set_title("conv(u, [-1/2, 0, 1/2])")
+
+    Kxu = Kx.matmul(u).reshape((height, width))
+    ax[1].imshow(Kxu, cmap='gray')
+    ax[1].set_title("Kx * u")
+
+    # ----------- Gradient Y direction -----------
+    # once with convolution, once with bigass op matrix
+    Ky = get_Ky(width, height)
+
+    cy = torch.Tensor([[0, -1/2, 0],
+                       [0, 0, 0],
+                       [0, 1/2, 0]]).unsqueeze(0).unsqueeze(0)
+
+    conv_y_u = F.conv2d(u.reshape(1, 1, width, height), cy, bias=None)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(conv_y_u.squeeze(), cmap='gray')
+    ax[0].set_title("conv(x, [-1/2, 0, 1/2].T)")
+
+    ax[1].imshow(Ky.matmul(u).reshape((height, width)), cmap='gray')
+    ax[1].set_title("Ky * u")
+
     plt.show()
+
+if __name__ == "__main__":
+    print("Coherence Enhancing Diffusion with parameters ...")
+    test_grad_operator()
